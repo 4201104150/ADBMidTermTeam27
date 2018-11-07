29b5606e43b8c52269ffe57befbd8ac20500ad47
= <hoami0405@gmail.com>
2018-11-07 10:19:43 +0700

    update cau 2

--- K40104070.sql
+++ K40104070.sql
@@ -13,7 +13,7 @@ INSERT INTO QLD (MaMucDich, YnghiaMucDich, MatDoLuongNuocTuoiTieu, MatDoDoanhThu
 VALUES ('M7', N'Trồng Lúa', 45000, 250.6, geometry::STGeomFromText('POLYGON ((-7 2, 4 2, 2 1, 4 0, -7 0, -5 1, -7 2))',0)),
 	   ('M8', N'Trồng Cây Ăn Quả', 30000, 350.4, geometry::STGeomFromText('POLYGON ((4 2, 0 2, 3 9, 8 6, 5 7, 4 2))',0)),
 	   ('M9', N'Trồng Rau', 60000, 450.5, geometry::STGeomFromText('POLYGON ((4 2, 2 1, 4 0, 7 -5, 8 -4, 9 1, 4 2))',0)),
-	   ('M10', N'Xoài', 75000, 400.5, geometry::STGeomFromText('POLYGON ((4 2, 5 7, 8 6, 9 1, 4 2))',0));
+	   ('M10', N'Trồng Xoài', 75000, 400.5, geometry::STGeomFromText('POLYGON ((4 2, 5 7, 8 6, 9 1, 4 2))',0));
 SELECT * FROM QLD;
 
 /* câu 2 */
